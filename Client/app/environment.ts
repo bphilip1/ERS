41d19fa222cfeae0f2fe4d047a2648e266fd095d
Bino Philip <binophilip123@gmail.com>
2018-06-18 11:18:29 -0400

    jenkins,environment files

---
+++ Client/app/environment.ts
@@ -0,0 +1,9 @@
+const dev = {
+  context: 'http://localhost:3001/'
+};
+
+const prod = {
+  context: 'http://ec2-18-216-129-244.us-east-2.compute.amazonaws.com:3001/'
+};
+
+export const environment = process.env.NODE_ENV === 'production' ? prod : dev;
